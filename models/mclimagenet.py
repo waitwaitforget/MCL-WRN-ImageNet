e4362b075e9eff71227ef37c86b573901d572599
waitwaitforget <tiank311@gmail.com>
2018-02-27 00:57:39 -0500

    push

--- models/mclimagenet.py
+++ models/mclimagenet.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 from wideresnet import WideResNet
 
 class MCLImageNet(nn.Module):
-	def __init__(self, name, nmodel):
+	def __init__(self, name, nmodel, nclasses=1000, widefactor=2):
 		super(MCLImageNet, self).__init__()
-		nclasses = 1000
-		widefactor = 2
+		nclasses = nclasses
+		widefactor = widefactor
 		droprate = 0.3
 		self.nmodel = nmodel
 
-		if model == 'wrn':
+		if name == 'wrn':
 			self.model = nn.ModuleList([WideResNet(18, nclasses, widefactor, droprate) for _ in range(nmodel)])
 
 	def forward(self, x):
